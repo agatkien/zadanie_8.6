ce62a58a5c6d202c8c2dc2dcd93ab135e4b5dc1f
Agata Snoch <agatasnoch@Agatas-MacBook-Air.local>
2017-08-07 17:13:14 +0400

    poprawa zadania

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ var a, b, value;
 a = 5;
 b = 6;
 value = (a * a) + (2 * a * b) - (b * b);
-console.log(value);
+
 
  
 
 if ( value > 0 ) {
-    console.log(value + 'wynik dodatni');
+    console.log(value + ' wynik dodatni');
 }
-  else if (value < 0) {
-      console.log(value + 'wynik ujemny');
-  }
-  else (value == 0) {
-       console.log(value);
-  }
+else if (value < 0) {
+    console.log(value + ' wynik ujemny');
+}
+else if (value == 0) {
+    console.log(value);
+}
        
